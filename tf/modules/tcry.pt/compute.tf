086ecd2f463781031928167c65d83481040efec7
Tyler Smith <tylersmith.me@gmail.com>
2020-11-07 17:40:45 -0800

    TWEAK: Remove debugging code now that cloud-config is fixed.

--- tf/modules/tcry.pt/compute.tf
+++ tf/modules/tcry.pt/compute.tf
@@ -2,11 +2,6 @@ data "template_file" "cloud_init" {
   template = file("${path.module}/scripts/cloud-init")
 }
 
-resource "digitalocean_ssh_key" "terraform" {
-  name       = "terraform"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO3YZKXgdbLQ5b0U3krcM/5vAwATXdWZorAsaqqNzPXy"
-}
-
 resource "digitalocean_droplet" "compute_beacon" {
   name               = "compute-beacon"
   ipv6               = true
@@ -20,7 +15,6 @@ resource "digitalocean_droplet" "compute_beacon" {
   size  = var.compute_beacon_instance_size
 
   user_data = data.template_file.cloud_init.rendered
-  ssh_keys  = [digitalocean_ssh_key.terraform.fingerprint]
 }
 
 resource "digitalocean_droplet" "compute_worker_1" {
@@ -38,6 +32,5 @@ resource "digitalocean_droplet" "compute_worker_1" {
   digitalocean_volume.storage_1.id]
 
   user_data = data.template_file.cloud_init.rendered
-  ssh_keys  = [digitalocean_ssh_key.terraform.fingerprint]
 }
 
